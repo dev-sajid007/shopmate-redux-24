a4fe9cedc9b52dbb613876393d34d94ebad99fd5
dev-sajid007 <dev.sajid007@gmail.com>
2024-07-18 17:41:56 +0600

    Project Complete

---
+++ src/components/ProductCard.js
@@ -0,0 +1,30 @@
+import "./ProductCard.css";
+import { useDispatch, useSelector } from "react-redux";
+import { add, remove } from "../store/cartSlice";
+import { useEffect, useState } from "react";
+export const ProductCard = ({ product }) => {
+  const { id, name, price, image } = product;
+  const dispatch = useDispatch();
+  const cartList = useSelector((state) => state.cartState.cartList);
+  const [isInCart, setIsInCart] = useState(false);
+
+  useEffect(() => {
+    const productIsInCart = cartList.find((item) => item.id === id);
+    if (productIsInCart) {
+      setIsInCart(true);
+    } else {
+      setIsInCart(false);
+    }
+  }, [cartList, id]);
+
+  return (
+    <div className="productCard">
+      <img src={image} alt={name} />
+      <p className="name">{name}</p>
+      <div className="action">
+        <p>${price}</p>
+        {isInCart ? (<button className="remove" onClick={() => dispatch(remove(product))}>Remove</button>) : (<button onClick={() => dispatch(add(product))}>Add To Cart</button>)}
+      </div>
+    </div>
+  );
+};
